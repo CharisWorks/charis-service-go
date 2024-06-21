954bde56d9e91cd7e15dc60f7af22c03b3c2e8ea
WhatACotton <whatacotton@gmail.com>
2024-06-21 15:50:09 +0900

    fix: add chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/charisworks/charisworks-service-go
 
 go 1.22.4
+
+require github.com/go-chi/chi v1.5.5
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
 func main() {
-	print("Hello World")
+	r := chi.NewRouter()
+	r.Get("/register", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("register"))
+	})
+	log.Print("server started")
+	http.ListenAndServe(":8080", r)
 }

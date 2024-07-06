e410423e7cf542647533277ea3d3f4b928e86574
WhatACotton <whatacotton@gmail.com>
2024-07-07 06:10:18 +0900

    fix: update models

--- meilisearch/models.go
+++ meilisearch/models.go
@@ -1,19 +1,20 @@
 package meilisearch
 
 type Item struct {
-	ID           string   `json:"id"`
-	ItemName     string   `json:"item_name"`
-	Price        int      `json:"price"`
-	Stock        int      `json:"stock"`
-	Description  string   `json:"description"`
-	Genre        string   `json:"genre"`
-	CreatedAt    string   `json:"createdAt"`
-	UpdatedAt    string   `json:"updatedAt"`
-	PublishedAt  string   `json:"publishedAt"`
-	Worker       string   `json:"worker"`
-	ThumbnailUrl string   `json:"thumbnail_url"`
-	Images       []Images `json:"images"`
-	Tags         []string `json:"tags"`
+	ID                string   `json:"id"`
+	ItemName          string   `json:"item_name"`
+	Price             int      `json:"price"`
+	Stock             int      `json:"stock"`
+	Description       string   `json:"description"`
+	Genre             string   `json:"genre"`
+	CreatedAt         string   `json:"createdAt"`
+	UpdatedAt         string   `json:"updatedAt"`
+	PublishedAt       string   `json:"publishedAt"`
+	Worker            string   `json:"worker"`
+	WorkerDescription string   `json:"worker_description"`
+	ThumbnailUrl      string   `json:"thumbnail_url"`
+	Images            []Images `json:"images"`
+	Tags              []string `json:"tags"`
 }
 
 type Images struct {
